67cfdbbd4eba0c8902072109c07016c34c084162
Kenrick <kenrick95@gmail.com>
2024-08-13 22:04:40 +0800

    feat: poc

--- src/Event/Activity.tsx
+++ src/Event/Activity.tsx
@@ -1,14 +1,46 @@
 import clsx from 'clsx';
 import style from './Activity.module.css';
+import tClasses from './Timeline.module.scss';
+
+const timeStartMapping: Record<string, string> = {};
+const timeEndMapping: Record<string, string> = {};
+
+function pad2(num: number): string {
+  if (num >= 10) {
+    return `${num}`;
+  }
+  return `0${num}`;
+}
+for (let i = 0; i < 24; i++) {
+  const hh = pad2(i);
+  for (let j = 0; j < 60; j++) {
+    const mm = pad2(j);
+    timeStartMapping[`${hh}${mm}`] = tClasses[`t${hh}${mm}`];
+    timeEndMapping[`${hh}${mm}`] = tClasses[`te${hh}${mm}`];
+  }
+}
 
 export function Activity({
   title,
   className,
+  timeStart,
+  timeEnd,
 }: {
   title: string;
   className?: string;
+  timeStart: string;
+  timeEnd: string;
 }) {
   return (
-    <div className={clsx(style.activity, className)}>Activity: {title}</div>
+    <div
+      className={clsx(
+        style.activity,
+        timeStartMapping[timeStart],
+        timeEndMapping[timeEnd],
+        className
+      )}
+    >
+      Activity: {title}
+    </div>
   );
 }
--- src/Event/Timeline.tsx
+++ src/Event/Timeline.tsx
@@ -1,65 +1,119 @@
 import clsx from 'clsx';
 import { Activity } from './Activity';
-import style from './Timeline.module.css';
-import tClasses from './Timeline.module.scss';
+import s from './Timeline.module.scss';
 
 const timeClassMapping = [
-  tClasses.t0000,
-  tClasses.t0100,
-  tClasses.t0200,
-  tClasses.t0300,
-  tClasses.t0400,
-  tClasses.t0500,
-  tClasses.t0600,
-  tClasses.t0700,
-  tClasses.t0800,
-  tClasses.t0900,
-  tClasses.t1000,
-  tClasses.t1100,
-  tClasses.t1200,
-  tClasses.t1300,
-  tClasses.t1400,
-  tClasses.t1500,
-  tClasses.t1600,
-  tClasses.t1700,
-  tClasses.t1800,
-  tClasses.t1900,
-  tClasses.t2000,
-  tClasses.t2100,
-  tClasses.t2200,
-  tClasses.t2300,
+  s.t0000,
+  s.t0100,
+  s.t0200,
+  s.t0300,
+  s.t0400,
+  s.t0500,
+  s.t0600,
+  s.t0700,
+  s.t0800,
+  s.t0900,
+  s.t1000,
+  s.t1100,
+  s.t1200,
+  s.t1300,
+  s.t1400,
+  s.t1500,
+  s.t1600,
+  s.t1700,
+  s.t1800,
+  s.t1900,
+  s.t2000,
+  s.t2100,
+  s.t2200,
+  s.t2300,
 ];
 const times = new Array(24).fill(0).map((_, i) => {
   return (
-    <TimelineTime className={timeClassMapping[i]} time={`${i}:00`} key={i} />
+    <TimelineTime
+      className={timeClassMapping[i]}
+      timeStart={`${i}:00`}
+      key={i}
+    />
   );
 });
 
 export function Timeline() {
   return (
-    <div className={tClasses.timeline}>
+    <div className={s.timeline}>
       {times}
-      <Activity className={style.timelineItem} title="Hotel" />
-      <Activity className={style.timelineItem} title="Breakfast" />
-      <Activity className={style.timelineItem} title="Walk" />
-      <Activity className={style.timelineItem} title="Lunch" />
-      <Activity className={style.timelineItem} title="Aquarium" />
-      <Activity className={style.timelineItem} title="Something" />
-      <Activity className={style.timelineItem} title="Zoo" />
-      <Activity className={style.timelineItem} title="Dinner" />
-      <Activity className={style.timelineItem} title="Museum" />
-      <Activity className={style.timelineItem} title="Nightclub" />
-      <Activity className={style.timelineItem} title="Hotel" />
+      <Activity
+        className={s.timelineItem}
+        // Hmmm, it's very error prone to type this 0000, 0800
+        timeStart="0000"
+        timeEnd="0800"
+        title="Hotel"
+      />
+      <Activity
+        className={s.timelineItem}
+        timeStart="0800"
+        timeEnd="0900"
+        title="Breakfast"
+      />
+      <Activity
+        className={s.timelineItem}
+        timeStart="0900"
+        timeEnd="0930"
+        title="Walk"
+      />
+      <Activity
+        className={s.timelineItem}
+        timeStart="1145"
+        timeEnd="1235"
+        title="Lunch"
+      />
+      <Activity
+        className={s.timelineItem}
+        timeStart="1245"
+        timeEnd="1400"
+        title="Aquarium"
+      />
+      <Activity
+        timeStart="1434"
+        timeEnd="1800"
+        className={s.timelineItem}
+        title="Zoo"
+      />
+      {/* Interesting... if overlap, CSS grid will show it to the next column */}
+      <Activity
+        className={s.timelineItem}
+        timeStart="1723"
+        timeEnd="1859"
+        title="Dinner"
+      />
+      <Activity
+        className={s.timelineItem}
+        timeStart="1900"
+        timeEnd="2123"
+        title="Museum"
+      />
+      <Activity
+        className={s.timelineItem}
+        timeStart="2200"
+        timeEnd="2300"
+        title="Nightclub"
+      />
+      <Activity
+        className={s.timelineItem}
+        timeStart="2300"
+        timeEnd="2359"
+        title="Hotel"
+      />
     </div>
   );
 }
 
 function TimelineTime({
   className,
-  time,
+  timeStart: time,
 }: {
   className: string;
-  time: string;
+  timeStart: string;
 }) {
-  return <div className={clsx(style.timelineTime, className)}>{time}</div>;
+  return <div className={clsx(s.timelineTime, className)}>{time}</div>;
 }
