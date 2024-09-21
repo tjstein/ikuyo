13d288299875d2e76db0bcb7019c6cdbb36e3678
Kenrick <kenrick95@gmail.com>
2024-09-21 09:36:26 +0800

    perf: drop module css for time/day classes

--- src/Event/TimelineStyles.ts
+++ src/Event/TimelineStyles.ts
@@ -1,5 +1,5 @@
 import { pad2 } from './time';
-import tClasses from './Timeline.module.scss';
+import './TimelineTimes.scss';
 
 export const timeStartMapping: Record<string, string> = {};
 export const timeEndMapping: Record<string, string> = {};
@@ -8,44 +8,44 @@ export const dayColMapping: Record<string, Record<string, string>> = {};
 export const dayEndMapping: Record<string, string> = {};
 
 export const timeColumnMapping = [
-  [tClasses[`t0000`], tClasses[`te0059`]],
-  [tClasses[`t0100`], tClasses[`te0159`]],
-  [tClasses[`t0200`], tClasses[`te0259`]],
-  [tClasses[`t0300`], tClasses[`te0359`]],
-  [tClasses[`t0400`], tClasses[`te0459`]],
-  [tClasses[`t0500`], tClasses[`te0559`]],
-  [tClasses[`t0600`], tClasses[`te0659`]],
-  [tClasses[`t0700`], tClasses[`te0759`]],
-  [tClasses[`t0800`], tClasses[`te0859`]],
-  [tClasses[`t0900`], tClasses[`te0959`]],
-  [tClasses[`t1000`], tClasses[`te1059`]],
-  [tClasses[`t1100`], tClasses[`te1159`]],
-  [tClasses[`t1200`], tClasses[`te1259`]],
-  [tClasses[`t1300`], tClasses[`te1359`]],
-  [tClasses[`t1400`], tClasses[`te1459`]],
-  [tClasses[`t1500`], tClasses[`te1559`]],
-  [tClasses[`t1600`], tClasses[`te1659`]],
-  [tClasses[`t1700`], tClasses[`te1759`]],
-  [tClasses[`t1800`], tClasses[`te1859`]],
-  [tClasses[`t1900`], tClasses[`te1959`]],
-  [tClasses[`t2000`], tClasses[`te2059`]],
-  [tClasses[`t2100`], tClasses[`te2159`]],
-  [tClasses[`t2200`], tClasses[`te2259`]],
-  [tClasses[`t2300`], tClasses[`te2359`]],
+  [`t0000`, `te0059`],
+  [`t0100`, `te0159`],
+  [`t0200`, `te0259`],
+  [`t0300`, `te0359`],
+  [`t0400`, `te0459`],
+  [`t0500`, `te0559`],
+  [`t0600`, `te0659`],
+  [`t0700`, `te0759`],
+  [`t0800`, `te0859`],
+  [`t0900`, `te0959`],
+  [`t1000`, `te1059`],
+  [`t1100`, `te1159`],
+  [`t1200`, `te1259`],
+  [`t1300`, `te1359`],
+  [`t1400`, `te1459`],
+  [`t1500`, `te1559`],
+  [`t1600`, `te1659`],
+  [`t1700`, `te1759`],
+  [`t1800`, `te1859`],
+  [`t1900`, `te1959`],
+  [`t2000`, `te2059`],
+  [`t2100`, `te2159`],
+  [`t2200`, `te2259`],
+  [`t2300`, `te2359`],
 ];
 for (let i = 0; i < 24; i++) {
   const hh = pad2(i);
   for (let j = 0; j < 60; j++) {
     const mm = pad2(j);
-    timeStartMapping[`${hh}${mm}`] = tClasses[`t${hh}${mm}`];
-    timeEndMapping[`${hh}${mm}`] = tClasses[`te${hh}${mm}`];
+    timeStartMapping[`${hh}${mm}`] = `t${hh}${mm}`;
+    timeEndMapping[`${hh}${mm}`] = `te${hh}${mm}`;
   }
 }
 for (let i = 0; i < 30; i++) {
-  dayStartMapping[i] = tClasses[`d${String(i)}`];
+  dayStartMapping[i] = `d${String(i)}`;
   dayColMapping[i] = {};
   for (let j = 0; j < 10; j++) {
-    dayColMapping[i][j] = tClasses[`d${String(i)}-c${String(j)}`];
+    dayColMapping[i][j] = `d${String(i)}-c${String(j)}`;
   }
-  dayEndMapping[i] = tClasses[`de${String(i)}`];
+  dayEndMapping[i] = `de${String(i)}`;
 }
