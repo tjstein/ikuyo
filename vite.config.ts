c2172a2442a868b3b0ca58f8655706d6c7c63ddd
Kenrick <kenrick95@gmail.com>
2024-09-21 10:02:28 +0800

    perf: split vendor chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes('node_modules')) {
+            if (id.includes('luxon')) {
+              return 'v-luxon';
+            }
+            if (id.includes('@radix-ui')) {
+              return 'v-radix-ui';
+            }
+            if (id.includes('@instantdb')) {
+              return 'v-instantdb';
+            }
+            if (id.includes('.pnpm/react') || id.includes('.pnpm/scheduler')) {
+              return 'v-react';
+            }
+            return 'v-vendor';
+          }
+        },
+      },
+    },
+  },
+});
