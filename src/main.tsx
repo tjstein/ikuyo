dae9842dad8e284e9c83ba670ba7a1041b8c8838
Kenrick <kenrick95@gmail.com>
2024-09-28 14:03:44 +0800

    feat(sw): immediate update

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
+/// <reference types="vite-plugin-pwa/client" />
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
- 
+import { registerSW } from 'virtual:pwa-register';
+
+registerSW({ immediate: true });
+
 createRoot(document.getElementById('root') as HTMLDivElement).render(
   <StrictMode>
     <App />
