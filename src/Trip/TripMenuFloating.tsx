14ccf85bb20238d84cc46749cd57933f414421b1
Kenrick <kenrick95@gmail.com>
2025-04-25 22:01:17 +0800

    feat: tweak ui

--- src/Trip/TripMenuFloating.tsx
+++ src/Trip/TripMenuFloating.tsx
@@ -7,46 +7,52 @@ import s from './TripMenuFloating.module.css';
 export function TripMenuFloating() {
   const [location, setLocation] = useLocation();
   return (
-    <nav className={s.menuFloatingContainer}>
-      <SegmentedControl.Root
-        defaultValue={location}
-        value={location}
-        className={s.menuFloating}
-        onValueChange={(value) => {
-          setLocation(value);
-        }}
-      >
-        <Tooltip content="Timetable view">
-          <SegmentedControl.Item
-            value={ROUTES_TRIP.TimetableView}
-            data-state={
-              location === (ROUTES_TRIP.TimetableView as string) ? 'on' : 'off'
-            }
-          >
-            <CalendarIcon className={s.navMenuIcon} />
-          </SegmentedControl.Item>
-        </Tooltip>
-        <Tooltip content="List view">
-          <SegmentedControl.Item
-            value={ROUTES_TRIP.ListView}
-            data-state={
-              location === (ROUTES_TRIP.ListView as string) ? 'on' : 'off'
-            }
-          >
-            <ListBulletIcon className={s.navMenuIcon} />
-          </SegmentedControl.Item>
-        </Tooltip>
-        <Tooltip content="Expenses">
-          <SegmentedControl.Item
-            value={ROUTES_TRIP.Expenses}
-            data-state={
-              location === (ROUTES_TRIP.Expenses as string) ? 'on' : 'off'
-            }
-          >
-            <TableIcon className={s.navMenuIcon} />
-          </SegmentedControl.Item>
-        </Tooltip>
-      </SegmentedControl.Root>
+    <nav className={s.nav}>
+      <div className={s.controlContainer}>
+        <SegmentedControl.Root
+          defaultValue={location}
+          value={location}
+          size="3"
+          variant="classic"
+          className={s.control}
+          onValueChange={(value) => {
+            setLocation(value);
+          }}
+        >
+          <Tooltip content="Timetable view">
+            <SegmentedControl.Item
+              value={ROUTES_TRIP.TimetableView}
+              data-state={
+                location === (ROUTES_TRIP.TimetableView as string)
+                  ? 'on'
+                  : 'off'
+              }
+            >
+              <CalendarIcon className={s.controlIcon} />
+            </SegmentedControl.Item>
+          </Tooltip>
+          <Tooltip content="List view">
+            <SegmentedControl.Item
+              value={ROUTES_TRIP.ListView}
+              data-state={
+                location === (ROUTES_TRIP.ListView as string) ? 'on' : 'off'
+              }
+            >
+              <ListBulletIcon className={s.controlIcon} />
+            </SegmentedControl.Item>
+          </Tooltip>
+          <Tooltip content="Expenses">
+            <SegmentedControl.Item
+              value={ROUTES_TRIP.Expenses}
+              data-state={
+                location === (ROUTES_TRIP.Expenses as string) ? 'on' : 'off'
+              }
+            >
+              <TableIcon className={s.controlIcon} />
+            </SegmentedControl.Item>
+          </Tooltip>
+        </SegmentedControl.Root>
+      </div>
     </nav>
   );
 }
