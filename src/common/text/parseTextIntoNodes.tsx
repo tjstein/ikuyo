e1b9cfab26abc3a2054e424ebc0c7ccc705db149
Kenrick <kenrick95@gmail.com>
2025-04-30 20:50:47 +0800

    fix: set key in part fragments

--- src/common/text/parseTextIntoNodes.tsx
+++ src/common/text/parseTextIntoNodes.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { Fragment, useMemo } from 'react';
 import createUrlRegExp from 'url-regex-safe';
 
 export function useParseTextIntoNodes(text: string): React.ReactNode[] {
@@ -19,23 +19,38 @@ export function parseTextIntoNodes(text: string): React.ReactNode[] {
 
   const parts: Array<React.ReactNode> = [];
   let i = 0;
+  let partIndex = 0;
   for (const match of matchArray) {
     const url = match[0];
     if (url) {
       const partBeforeUrl = receivedText.slice(i, match.index);
-      parts.push(partBeforeUrl);
+      if (partBeforeUrl.length > 0) {
+        parts.push(<Fragment key={partIndex}>{partBeforeUrl}</Fragment>);
+        partIndex += 1;
+      }
       if (!url.startsWith('http')) {
-        parts.push(url);
+        parts.push(<Fragment key={partIndex}>{url}</Fragment>);
+        partIndex += 1;
       } else {
         parts.push(
-          <a href={url} target="_blank" rel="noopener noreferrer">
+          <a
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            key={partIndex}
+          >
             {url}
           </a>,
         );
+        partIndex += 1;
       }
       i = match.index + url.length;
     }
   }
-  parts.push(receivedText.slice(i, receivedText.length));
+  const finalPart = receivedText.slice(i, receivedText.length);
+  if (finalPart.length > 0) {
+    parts.push(<Fragment key={partIndex}>{finalPart}</Fragment>);
+    partIndex += 1;
+  }
   return parts;
 }
