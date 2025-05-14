ebbbfa354d53d88b3b152c9b52aff1c9ed26911c
Kenrick <kenrick95@gmail.com>
2025-05-14 22:28:50 +0800

    feat: context menu in activity list

--- src/ActivityList/ActivityList.tsx
+++ src/ActivityList/ActivityList.tsx
@@ -1,13 +1,17 @@
-import { Flex, Heading } from '@radix-ui/themes';
-import { useMemo } from 'react';
+import { ContextMenu, Flex, Heading } from '@radix-ui/themes';
+import { useCallback, useMemo } from 'react';
 import { Route, Switch } from 'wouter';
 import { Accommodation } from '../Accommodation/Accommodation';
 import { AccommodationDialog } from '../Accommodation/AccommodationDialog';
+import { AccommodationNewDialog } from '../Accommodation/AccommodationNewDialog';
 import { Activity } from '../Activity/Activity';
 import { ActivityDialog } from '../Activity/ActivityDialog';
+import { ActivityNewDialog } from '../Activity/ActivityNewDialog';
 import { groupActivitiesByDays } from '../Activity/eventGrouping';
+import { useBoundStore } from '../data/store';
 import { Macroplan } from '../Macroplan/Macroplan';
 import { MacroplanDialog } from '../Macroplan/MacroplanDialog';
+import { MacroplanNewDialog } from '../Macroplan/MacroplanNewDialog';
 import {
   RouteTripListViewAccommodation,
   RouteTripListViewActivity,
@@ -21,7 +25,23 @@ import s from './ActivityList.module.css';
 export function ActivityList() {
   const trip = useTrip();
   const dayGroups = useMemo(() => groupActivitiesByDays(trip), [trip]);
+  const pushDialog = useBoundStore((state) => state.pushDialog);
+  const openActivityNewDialog = useCallback(() => {
+    () => {
+      pushDialog(ActivityNewDialog, { trip });
+    };
+  }, [pushDialog, trip]);
+  const openAccommodationNewDialog = useCallback(() => {
+    () => {
+      pushDialog(AccommodationNewDialog, { trip });
+    };
+  }, [pushDialog, trip]);
 
+  const openMacroplanNewDialog = useCallback(() => {
+    () => {
+      pushDialog(MacroplanNewDialog, { trip });
+    };
+  }, [pushDialog, trip]);
   return (
     <>
       <Flex
@@ -29,67 +49,85 @@ export function ActivityList() {
         justify="between"
         direction={{ initial: 'column', sm: 'row' }}
       >
-        <Flex
-          className={s.list}
-          direction="column"
-          gap="2"
-          flexGrow="1"
-          maxWidth={{ initial: '100%', sm: '50%' }}
-        >
-          {dayGroups.map((dayGroup) => {
-            return [
-              <Heading
-                key={dayGroup.startDateTime.toISO()}
-                as="h2"
-                size="4"
-                className={s.listSubheader}
-              >
-                {dayGroup.startDateTime.toFormat('cccc, dd LLLL yyyy')}
-              </Heading>,
-              ...Object.values(dayGroup.macroplans).map((macroplan, i) => {
-                return (
-                  <Macroplan
-                    key={`${macroplan.id}-${String(i)}`}
-                    macroplan={macroplan}
-                    className={s.listItem}
-                    tripViewMode={TripViewMode.List}
-                  />
-                );
-              }),
-              ...Object.values(dayGroup.accommodations).map(
-                (accommodation, i) => {
-                  const props = dayGroup.accommodationProps.get(
-                    accommodation.id,
-                  );
-                  return (
-                    <Accommodation
-                      key={`${accommodation.id}-${String(i)}`}
-                      accommodation={accommodation}
-                      tripViewMode={TripViewMode.List}
-                      className={s.listItem}
-                      {...props}
-                    />
-                  );
-                },
-              ),
-              ...Object.values(dayGroup.activities).map((activity) => {
-                const columnIndex = dayGroup.activityColumnIndexMap.get(
-                  activity.id,
-                );
-                return (
-                  <Activity
-                    key={activity.id}
-                    className={s.listItem}
-                    activity={activity}
-                    columnIndex={columnIndex?.start ?? 1}
-                    columnEndIndex={columnIndex?.end ?? 1}
-                    tripViewMode={TripViewMode.List}
-                  />
-                );
-              }),
-            ];
-          })}
-        </Flex>
+        <ContextMenu.Root>
+          <ContextMenu.Trigger>
+            <Flex
+              className={s.list}
+              direction="column"
+              gap="2"
+              flexGrow="1"
+              maxWidth={{ initial: '100%', sm: '50%' }}
+            >
+              {dayGroups.map((dayGroup) => {
+                return [
+                  <Heading
+                    key={dayGroup.startDateTime.toISO()}
+                    as="h2"
+                    size="4"
+                    className={s.listSubheader}
+                  >
+                    {dayGroup.startDateTime.toFormat('cccc, dd LLLL yyyy')}
+                  </Heading>,
+                  ...Object.values(dayGroup.macroplans).map((macroplan, i) => {
+                    return (
+                      <Macroplan
+                        key={`${macroplan.id}-${String(i)}`}
+                        macroplan={macroplan}
+                        className={s.listItem}
+                        tripViewMode={TripViewMode.List}
+                      />
+                    );
+                  }),
+                  ...Object.values(dayGroup.accommodations).map(
+                    (accommodation, i) => {
+                      const props = dayGroup.accommodationProps.get(
+                        accommodation.id,
+                      );
+                      return (
+                        <Accommodation
+                          key={`${accommodation.id}-${String(i)}`}
+                          accommodation={accommodation}
+                          tripViewMode={TripViewMode.List}
+                          className={s.listItem}
+                          {...props}
+                        />
+                      );
+                    },
+                  ),
+                  ...Object.values(dayGroup.activities).map((activity) => {
+                    const columnIndex = dayGroup.activityColumnIndexMap.get(
+                      activity.id,
+                    );
+                    return (
+                      <Activity
+                        key={activity.id}
+                        className={s.listItem}
+                        activity={activity}
+                        columnIndex={columnIndex?.start ?? 1}
+                        columnEndIndex={columnIndex?.end ?? 1}
+                        tripViewMode={TripViewMode.List}
+                      />
+                    );
+                  }),
+                ];
+              })}
+            </Flex>
+          </ContextMenu.Trigger>
+          <ContextMenu.Content>
+            <ContextMenu.Label>{trip.title}</ContextMenu.Label>
+            <ContextMenu.Item onClick={openActivityNewDialog}>
+              New activity
+            </ContextMenu.Item>
+
+            <ContextMenu.Item onClick={openAccommodationNewDialog}>
+              New acommodation
+            </ContextMenu.Item>
+
+            <ContextMenu.Item onClick={openMacroplanNewDialog}>
+              New day plan
+            </ContextMenu.Item>
+          </ContextMenu.Content>
+        </ContextMenu.Root>
         <Flex
           direction="column"
           gap="2"
