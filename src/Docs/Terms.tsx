6cec59654454c6cbac768b937265c3c1beb8639a
Kenrick <kenrick95@gmail.com>
2025-05-27 21:39:03 +0800

    feat: generate tos

--- src/Docs/Terms.tsx
+++ src/Docs/Terms.tsx
@@ -1,9 +1,321 @@
 export default function Terms() {
   return (
     <>
-      <h1>Terms of Use</h1>
-      <p>Last updated: 10 November 2024</p>
-      <p>To be updated</p>
+      <h1>Terms of Service</h1>
+      <p>Last updated: 27 May 2025</p>
+
+      <h2>Agreement to Terms</h2>
+      <p>
+        These Terms of Service ("Terms") govern your use of our website and
+        application Ikuyo located at https://ikuyo.kenrick95.org (the "Service")
+        operated by Ikuyo ("us", "we", or "our").
+      </p>
+      <p>
+        By accessing and using our Service, you accept and agree to be bound by
+        the terms and provision of this agreement. If you do not agree to abide
+        by the above, please do not use this service.
+      </p>
+
+      <h2>Definitions</h2>
+      <p>For the purposes of these Terms of Service:</p>
+      <ul>
+        <li>
+          <p>
+            <strong>Account</strong> means a unique account created for you to
+            access our Service or parts of our Service.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Company</strong> (referred to as either "the Company", "we",
+            "us" or "our") refers to Ikuyo.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Service</strong> refers to the Ikuyo website and
+            application.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Terms of Service</strong> (also referred as "Terms") mean
+            these Terms of Service that form the entire agreement between you
+            and the Company regarding the use of the Service.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>You</strong> refers to the individual accessing or using the
+            Service, or the company, or other legal entity on behalf of which
+            such individual is accessing or using the Service, as applicable.
+          </p>
+        </li>
+      </ul>
+
+      <h2>Use License</h2>
+      <p>
+        Permission is granted to temporarily access and use the materials on
+        Ikuyo's website and application for personal, non-commercial transitory
+        viewing only. This is the grant of a license, not a transfer of title,
+        and under this license you may not:
+      </p>
+      <ul>
+        <li>modify or copy the materials;</li>
+        <li>
+          use the materials for any commercial purpose or for any public display
+          (commercial or non-commercial);
+        </li>
+        <li>
+          attempt to decompile or reverse engineer any software contained on
+          Ikuyo's website or application;
+        </li>
+        <li>
+          remove any copyright or other proprietary notations from the
+          materials.
+        </li>
+      </ul>
+      <p>
+        This license shall automatically terminate if you violate any of these
+        restrictions and may be terminated by Ikuyo at any time. Upon
+        terminating your viewing of these materials or upon the termination of
+        this license, you must destroy any downloaded materials in your
+        possession whether in electronic or printed format.
+      </p>
+
+      <h2>User Accounts</h2>
+      <p>
+        When you create an account with us, you must provide information that is
+        accurate, complete, and current at all times. Incomplete, inaccurate, or
+        obsolete information may result in the immediate termination of your
+        account on the Service.
+      </p>
+      <p>
+        You are responsible for safeguarding the password that you use to access
+        the Service and for all activities that occur under your account,
+        whether your password is with our Service or a third-party service.
+      </p>
+      <p>
+        You agree not to disclose your password to any third party. You must
+        notify us immediately upon becoming aware of any breach of security or
+        unauthorized use of your account.
+      </p>
+
+      <h2>Acceptable Use</h2>
+      <p>You agree not to use the Service:</p>
+      <ul>
+        <li>
+          for any unlawful purpose or to solicit others to perform unlawful
+          acts;
+        </li>
+        <li>
+          to violate any international, federal, provincial, or state
+          regulations, rules, laws, or local ordinances;
+        </li>
+        <li>
+          to infringe upon or violate our intellectual property rights or the
+          intellectual property rights of others;
+        </li>
+        <li>
+          to harass, abuse, insult, harm, defame, slander, disparage,
+          intimidate, or discriminate;
+        </li>
+        <li>to submit false or misleading information;</li>
+        <li>
+          to upload or transmit viruses or any other type of malicious code;
+        </li>
+        <li>to collect or track the personal information of others;</li>
+        <li>to spam, phish, pharm, pretext, spider, crawl, or scrape;</li>
+        <li>for any obscene or immoral purpose; or</li>
+        <li>
+          to interfere with or circumvent the security features of the Service.
+        </li>
+      </ul>
+      <p>
+        We reserve the right to terminate your use of the Service for violating
+        any of the prohibited uses.
+      </p>
+
+      <h2>User-Generated Content</h2>
+      <p>
+        Our Service may allow you to post, link, store, share and otherwise make
+        available certain information, text, graphics, videos, or other material
+        ("Content"). You are responsible for the Content that you post to the
+        Service, including its legality, reliability, and appropriateness.
+      </p>
+      <p>By posting Content to the Service, you grant us the right to:</p>
+      <ul>
+        <li>
+          use, modify, publicly perform, publicly display, reproduce, and
+          distribute such Content on and through the Service;
+        </li>
+        <li>use and disclose your identity in connection with such Content;</li>
+        <li>
+          use such Content for our business purposes, including for promoting
+          and redistributing part or all of the Service.
+        </li>
+      </ul>
+      <p>
+        You represent and warrant that: (i) the Content is yours (you own it) or
+        you have the right to use it and grant us the rights and license as
+        provided in these Terms, and (ii) your use of the Content does not
+        infringe, violate or misappropriate the rights of any third party.
+      </p>
+
+      <h2>Privacy Policy</h2>
+      <p>
+        Your privacy is important to us. Our Privacy Policy explains how we
+        collect, use, and protect your information when you use our Service. By
+        using our Service, you agree to the collection and use of information in
+        accordance with our Privacy Policy.
+      </p>
+
+      <h2>Prohibited Uses</h2>
+      <p>
+        In addition to other prohibitions as set forth in the Terms of Service,
+        you are prohibited from using the site or its content:
+      </p>
+      <ul>
+        <li>
+          for any unlawful purpose or to solicit others to perform unlawful
+          acts;
+        </li>
+        <li>
+          to violate any international, federal, provincial, or state
+          regulations, rules, laws, or local ordinances;
+        </li>
+        <li>
+          to infringe upon or violate our intellectual property rights or the
+          intellectual property rights of others;
+        </li>
+        <li>
+          to harass, abuse, insult, harm, defame, slander, disparage,
+          intimidate, or discriminate based on gender, sexual orientation,
+          religion, ethnicity, race, age, national origin, or disability;
+        </li>
+        <li>to submit false or misleading information;</li>
+        <li>
+          to upload or transmit viruses or any other type of malicious code that
+          will or may be used in any way that will affect the functionality or
+          operation of the Service or of any related website, other websites, or
+          the Internet;
+        </li>
+        <li>to collect or track the personal information of others;</li>
+        <li>to spam, phish, pharm, pretext, spider, crawl, or scrape;</li>
+        <li>for any obscene or immoral purpose; or</li>
+        <li>
+          to interfere with or circumvent the security features of the Service
+          or any related website, other websites, or the Internet.
+        </li>
+      </ul>
+
+      <h2>Service Availability</h2>
+      <p>
+        Our Service is provided on an "as is" and "as available" basis. Ikuyo
+        makes no warranties, expressed or implied, and hereby disclaims and
+        negates all other warranties including without limitation, implied
+        warranties or conditions of merchantability, fitness for a particular
+        purpose, or non-infringement of intellectual property or other violation
+        of rights.
+      </p>
+      <p>
+        We do not warrant that the Service will be uninterrupted, secure, or
+        error-free, that defects will be corrected, or that the Service or the
+        server that makes it available are free of viruses or other harmful
+        components.
+      </p>
+
+      <h2>Limitation of Liability</h2>
+      <p>
+        In no event shall Ikuyo or its suppliers be liable for any damages
+        (including, without limitation, damages for loss of data or profit, or
+        due to business interruption) arising out of the use or inability to use
+        the materials on Ikuyo's website or application, even if Ikuyo or an
+        authorized representative has been notified orally or in writing of the
+        possibility of such damage. Because some jurisdictions do not allow
+        limitations on implied warranties, or limitations of liability for
+        consequential or incidental damages, these limitations may not apply to
+        you.
+      </p>
+
+      <h2>Indemnification</h2>
+      <p>
+        You agree to defend, indemnify, and hold harmless Ikuyo and its licensee
+        and licensors, and their employees, contractors, agents, officers and
+        directors, from and against any and all claims, damages, obligations,
+        losses, liabilities, costs or debt, and expenses (including but not
+        limited to attorney's fees).
+      </p>
+
+      <h2>Termination</h2>
+      <p>
+        We may terminate or suspend your account immediately, without prior
+        notice or liability, for any reason whatsoever, including without
+        limitation if you breach the Terms.
+      </p>
+      <p>
+        Upon termination, your right to use the Service will cease immediately.
+        If you wish to terminate your account, you may simply discontinue using
+        the Service.
+      </p>
+
+      <h2>Severability</h2>
+      <p>
+        If any provision of these Terms is held to be unenforceable or invalid,
+        such provision will be changed and interpreted to accomplish the
+        objectives of such provision to the greatest extent possible under
+        applicable law and the remaining provisions will continue in full force
+        and effect.
+      </p>
+
+      <h2>Waiver</h2>
+      <p>
+        Except as provided herein, the failure to exercise a right or to require
+        performance of an obligation under these Terms shall not effect a
+        party's ability to exercise such right or require such performance at
+        any time thereafter nor shall the waiver of a breach constitute a waiver
+        of any subsequent breach.
+      </p>
+
+      <h2>Changes to Terms</h2>
+      <p>
+        We reserve the right, at our sole discretion, to modify or replace these
+        Terms at any time. If a revision is material, we will try to provide at
+        least 30 days notice prior to any new terms taking effect.
+      </p>
+      <p>
+        By continuing to access or use our Service after those revisions become
+        effective, you agree to be bound by the revised terms. If you do not
+        agree to the new terms, please stop using the Service.
+      </p>
+
+      <h2>Governing Law</h2>
+      <p>
+        These Terms shall be interpreted and governed by the laws of Singapore,
+        without regard to its conflict of law provisions. Our failure to enforce
+        any right or provision of these Terms will not be considered a waiver of
+        those rights.
+      </p>
+
+      <h2>Contact Information</h2>
+      <p>
+        If you have any questions about these Terms of Service, please contact
+        us:
+      </p>
+      <ul>
+        <li>
+          <p>
+            By visiting this page on our website:{' '}
+            <a
+              href="https://ikuyo.kenrick95.org/"
+              rel="noreferrer external nofollow noopener"
+              target="_blank"
+            >
+              https://ikuyo.kenrick95.org/
+            </a>
+          </p>
+        </li>
+      </ul>
     </>
   );
 }
