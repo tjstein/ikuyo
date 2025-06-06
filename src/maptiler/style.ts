3fa01aa43a46bf93f3574533a19e5907944741e8
Kenrick <kenrick95@gmail.com>
2025-06-06 23:29:07 +0800

    feat(map): use own style even for light mode

--- src/maptiler/style.ts
+++ src/maptiler/style.ts
@@ -1,7 +1,6 @@
-import { MapStyle as MapStyleDefault } from '@maptiler/sdk';
-
 export const MapStyle = {
-  OPENSTREETMAP: MapStyleDefault.OPENSTREETMAP,
+  OPENSTREETMAP:
+    'https://api.maptiler.com/maps/019648d2-3f17-74c9-8b51-d89c119c3f7a/style.json',
   OPENSTREETMAP_DARK:
     'https://api.maptiler.com/maps/01970021-fd93-70ac-9af7-32acd4debb35/style.json',
 };
